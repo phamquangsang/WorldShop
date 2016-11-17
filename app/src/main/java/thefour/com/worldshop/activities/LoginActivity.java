ede448c8f938134a0dca38cf462b5307285d02a3
Pham Sang <phamngocquangsang@gmail.com>
2016-11-17 15:24:26 +0700

    finish login with google

--- app/src/main/java/thefour/com/worldshop/activities/LoginActivity.java
+++ app/src/main/java/thefour/com/worldshop/activities/LoginActivity.java
@@ -1,15 +1,179 @@
 package thefour.com.worldshop.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import thefour.com.worldshop.R;
+import thefour.com.worldshop.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 11;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ActivityLoginBinding mBinding;
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        final long timeBeginCreate = System.currentTimeMillis();
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        handleEvent();
+        //initialize Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.i(TAG, "onAuthStateChanged: log time need to check user"+ (System.currentTimeMillis()-timeBeginCreate));
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                mProgressDialog.dismiss();
+            }
+        };
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestProfile()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mProgressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setTitle(R.string.title_login_dialog);
+        mProgressDialog.setMessage(getString(R.string.message_login_dialog));
+    }
+
+    private void handleEvent() {
+        mBinding.googleSignInButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Snackbar.make(mBinding.loginContainer,"Cannot connect to Google!\n"
+                + connectionResult.getErrorMessage(),Snackbar.LENGTH_LONG);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google_sign_in_button:
+                mProgressDialog.show();
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
+            Toast.makeText(this, "login succeed" + "\nName:"+acct.getDisplayName()
+                    +"\nEmail:"+acct.getEmail()
+                    +"\nProfileImage:"+acct.getPhotoUrl()
+                    , Toast.LENGTH_SHORT).show();
+        } else {
+//            // Signed out, show unauthenticated UI.
+//            updateUI(false);
+            Log.e(TAG, "handleSignInResult: sign in failed");
+        }
+    }
+    public static Intent getIntent(Context c){
+        Intent i = new Intent(c, LoginActivity.class);
+        return i;
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/thefour/com/worldshop/activities/SplashActivity.java
+++ app/src/main/java/thefour/com/worldshop/activities/SplashActivity.java
@@ -4,32 +4,53 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import thefour.com.worldshop.R;
 import thefour.com.worldshop.animation.Animation;
 import thefour.com.worldshop.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
-    private ActivitySplashBinding mBinding;
     private static final String TAG = SplashActivity.class.getSimpleName();
-    private ImageView mFlashScreenImageView;
-    private TextView mTitleTextView;
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private static final int DURATION = 500;
+    private FirebaseUser mFirebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
-        mFlashScreenImageView = mBinding.imageViewFlashScreen;
-        mTitleTextView = mBinding.textViewFlashScreenTitle;
+        ActivitySplashBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
 
-        AnimatorSet zoomOutImage = Animation.getZoomAnimation(mFlashScreenImageView,DURATION,new AccelerateDecelerateInterpolator(),1f,3f);
-        AnimatorSet zoomOutTextView = Animation.getZoomAnimation(mTitleTextView,DURATION,new AccelerateDecelerateInterpolator(),1f,3f);
+
+        //initialize Firebase Authentication
+        final long timeBeginCreate = System.currentTimeMillis();
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.i(TAG, "onAuthStateChanged: log time need to check user: "
+                        + (System.currentTimeMillis()-timeBeginCreate));
+                mFirebaseUser = firebaseAuth.getCurrentUser();
+            }
+        };
+
+        AnimatorSet zoomOutImage = Animation.getZoomAnimation(mBinding.imageViewFlashScreen
+                ,DURATION, new AccelerateDecelerateInterpolator(),1f,3f);
+        AnimatorSet zoomOutTextView = Animation.getZoomAnimation(mBinding.textViewFlashScreenTitle
+                ,DURATION, new AccelerateDecelerateInterpolator(),1f,3f);
 
         AnimatorSet finalZoom = new AnimatorSet();
         finalZoom.setDuration(DURATION);
@@ -41,20 +62,30 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
-
-//                Gson gson = new Gson();
-//                String userJson = Utilities.getSharedPreference(FlashScreenActivity.this)
-//                        .getString(Constants.LOGGED_USER_JSON, "");
-//                User loggedUser = gson.fromJson(userJson, User.class);
-//                if (loggedUser == null || loggedUser.getEmail()== null) {
-//                    User temporaryUser = User.getTemporaryUser(FlashScreenActivity.this);
-//                    startActivity(IcychatActivity.newIntent(FlashScreenActivity.this, temporaryUser,true));
-//                }else{
-//                    startActivity(IcychatActivity.newIntent(FlashScreenActivity.this, loggedUser,true));
-//                }
+                if (mFirebaseUser != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + mFirebaseUser.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    startActivity(LoginActivity.getIntent(SplashActivity.this));
+                }
 
             }
         });
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
