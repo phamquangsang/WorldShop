a2ea12b173e496d3798a779c01e1db6a6b8b0d6a
Nhat Vu <minhnhatse.uit@gmail.com>
2016-11-22 17:47:57 +0700

    add picking photo drawable, permision Utils

---
+++ app/src/main/java/thefour/com/worldshop/PermissionUtils.java
@@ -0,0 +1,12 @@
+package thefour.com.worldshop;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by Quang Quang on 11/22/2016.
+ */
+
+public class PermissionUtils {
+
+}
