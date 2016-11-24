877d6dffdfc2bc4d29ed068ba1ce2d2efce8dba4
Pham Sang <phamngocquangsang@gmail.com>
2016-11-24 09:27:14 +0700

    finish citi api

--- app/src/main/java/thefour/com/worldshop/api/CityApi.java
+++ app/src/main/java/thefour/com/worldshop/api/CityApi.java
@@ -1,8 +1,44 @@
 package thefour.com.worldshop.api;
 
+import com.bumptech.glide.disklrucache.DiskLruCache;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import thefour.com.worldshop.Contracts;
+import thefour.com.worldshop.models.City;
+
 /**
  * Created by Quang Quang on 11/23/2016.
  */
 
 public class CityApi {
+    public interface OnLoadCompleted{
+        void onLoadCompleted(ArrayList<City> cities);
+        void onLoadCancelled(DatabaseError error);
+    }
+    public static void loadCities(final OnLoadCompleted onLoadCompleted){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(Contracts.CITY_LOCATION);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<City> cities = new ArrayList<City>();
+                for (DataSnapshot city :
+                        dataSnapshot.getChildren()) {
+                    City cityObject = city.getValue(City.class);
+                    cities.add(cityObject);
+                }
+                onLoadCompleted.onLoadCompleted(cities);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                onLoadCompleted.onLoadCancelled(databaseError);
+            }
+        });
+    }
 }
