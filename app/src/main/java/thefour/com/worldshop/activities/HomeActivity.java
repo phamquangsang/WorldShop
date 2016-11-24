8d06340e1f718bc6551bf7705bbb5f8b737e818c
Pham Sang <phamngocquangsang@gmail.com>
2016-11-24 09:29:37 +0700

    Home activity now require userId in getItent(). add loading loggedUser

--- app/src/main/java/thefour/com/worldshop/activities/HomeActivity.java
+++ app/src/main/java/thefour/com/worldshop/activities/HomeActivity.java
@@ -3,6 +3,7 @@ package thefour.com.worldshop.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,10 +11,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import thefour.com.worldshop.R;
+import thefour.com.worldshop.api.UserApi;
+import thefour.com.worldshop.models.User;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String ARG_USER_ID = "arg_user_id";
+    private User mLoggedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String userId = getIntent().getStringExtra(ARG_USER_ID);
+        if(userId!=null){
+            UserApi.retrieveUserById(userId, new UserApi.IsUserExistCallback() {
+                @Override
+                public void onUserExist(@Nullable User user) {
+                    if(user!=null)
+                        mLoggedUser = user;
+                    else{
+                        Toast.makeText(HomeActivity.this, "Please Login again!", Toast.LENGTH_SHORT).show();
+                        startActivity(LoginActivity.getIntent(HomeActivity.this));
+                    }
+                }
+            });
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +65,7 @@ public class HomeActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_shop:{
                 //TODO start shopping activity
-                startActivity(ShoppingActivity.getIntent(this));
+                startActivity(ShoppingActivity.getIntent(this, mLoggedUser));
                 break;
             }
             case R.id.action_travel:{
@@ -53,8 +75,9 @@ public class HomeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public static Intent getIntent(Context c){
+    public static Intent getIntent(Context c, String userId){
         Intent i = new Intent(c, HomeActivity.class);
+        i.putExtra(ARG_USER_ID, userId);
         return i;
     }
 }
